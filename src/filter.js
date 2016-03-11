b042f74d11ded13ac382a1807b53f130c6a5889c
AzuObs <dleaverfr@gmail.com>
2016-03-11 13:34:47 +0100

    filter can be register either by (filterName, filterFn) or (filtersObject) where the key of the object property is the name of the filter and the function is the filter

--- src/filter.js
+++ src/filter.js
@@ -4,9 +4,16 @@
   var filters = {};
 
   window.register = function(name, factory) {
-    var filter = factory();
-    filters[name] = filter;
-    return filter;
+    if (name && mixin.isObjectLike(name)) { // if name is an object
+      return Object.keys(name).map(function(key) { // map object
+        return register(key, name[key]);
+      });
+    }
+    else { // if name is a string
+      var filter = factory();
+      filters[name] = filter;
+      return filter;
+    }
   };
 
   window.filter = function(name) {
