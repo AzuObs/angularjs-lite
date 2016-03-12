59103b0b0f650963a1f4f43a459aeaca858230f3
AzuObs <dleaverfr@gmail.com>
2016-03-12 12:56:04 +0100

    Filter's filter can filter using a string. eg parse("['a','b','c'] | filter:'a'") returns ['a','a']

--- src/filter-filter.js
+++ src/filter-filter.js
@@ -1,11 +1,32 @@
 (function() {
   "use strict";
 
-  var filterFilter = function(array, fn) {
-    return function(array, filterExpr) {
-      return array.filter(filterExpr);
+
+  var createPredicateFn = function(expression) {
+    return function predicateFn(item) {
+      return item === expression;
     };
   };
+
+
+  var filterFilter = function(array, fn) {
+    return function(array, filterExpr) {
+      var predicateFn;
+
+      if (filterExpr) {
+        if (typeof filterExpr === "function") {
+          predicateFn = filterExpr;
+        }
+        else if (typeof filterExpr === "string") {
+          // debugger
+          predicateFn = createPredicateFn(filterExpr);
+        }
+      }
+
+      return array.filter(predicateFn);
+    };
+  };
+
 
   register("filter", filterFilter);
 })();
--- test/filter-filter-spec.js
+++ test/filter-filter-spec.js
@@ -8,14 +8,22 @@
     });
 
 
-    it('can filter an array with a predicate function', function() {
-      var fn = parse('[1, 2, 3, 4] | filter:isOdd');
+    it("can filter an array with a predicate function", function() {
+      var fn = parse("[1, 2, 3, 4] | filter:isOdd");
       var scope = {
         isOdd: function(n) {
           return n % 2 !== 0;
         }
       };
       expect(fn(scope)).toEqual([1, 3]);
+    });
+
+
+    it("can filter an array of strings with a string", function() {
+      var fn = parse("arr | filter:'a'");
+      expect(fn({
+        arr: ["a", "b", "a"]
+      })).toEqual(["a", "a"]);
     });
   });
 })();
