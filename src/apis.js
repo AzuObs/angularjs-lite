c71572be2e103f652846a6ddc84499611cb4b94e
AzuObs <dleaverfr@gmail.com>
2016-03-22 13:03:03 +0100

    for object/arrays/functions, hashKey will return the typeof and a uniqueID, the uniqueID is an integer that starts at 0 and goes up to Number.MAX_NUMBER

--- src/apis.js
+++ src/apis.js
@@ -3,7 +3,21 @@
 
   function hashKey(value) {
     var type = typeof value;
-    return type + ":" + value;
+    var uid;
+
+    if (type === "function" || (type === "object" && value !== null)) {
+      uid = value.$$hashKey;
+      if (uid === undefined) {
+        // _.uniqueId is simply a function that has an internal counter
+        // and increments it by one everytime it is called
+        // it's the same as what angular does internally
+        uid = value.$$hashKey = _.uniqueId();
+      }
+    }
+    else {
+      uid = value;
+    }
+    return type + ":" + uid;
   }
 
   window.hashKey = hashKey;
